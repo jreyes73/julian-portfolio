802e1cc13bcfa8b27327b9f4fa8ac6eaa3a9672c
Julian Reyes <julianreyes4@gmail.com>
2024-04-17 00:02:16 +0900

    add pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import React from 'react';
 import Root from './Root/Root';
 import Home from './components/Home/Home';
+import AboutMe from './components/AboutMe/AboutMe';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Root />} >
     <Route path='' element={<Home />} />
+    <Route path='aboutme' element={<AboutMe />} />
+    <Route path='projects' element={<Projects />} />
+    <Route path='contact' element={<Contact />} />
   </Route>
 ))
 
---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function AboutMe() {
+    return (
+        <h1>this is the about me tab.</h1>
+    )
+}
